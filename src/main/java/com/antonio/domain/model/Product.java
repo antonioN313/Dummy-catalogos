e018c2f1bf065d85f80a0ae727b02efe1b6ac98c
Antonio Carlos Fernandes Gomes Filho <antoniocarlosfilho2003@gmail.com>
2025-10-17 19:39:23 -0300

    feature: Product domain model

---
+++ src/main/java/com/antonio/domain/model/Product.java
@@ -0,0 +1,14 @@
+package com.antonio.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private Integer id;
+    private String titulo;
+    private Double preco;
+}
